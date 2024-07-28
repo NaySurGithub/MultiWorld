01f03dc19c26a9aed1604b47da90c8be658685d7
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2024-07-28 23:30:34 +0200

    Update OverworldGenerator.php

--- src/czechpmdevs/multiworld/generator/overworld/OverworldGenerator.php
+++ src/czechpmdevs/multiworld/generator/overworld/OverworldGenerator.php
@@ -41,7 +41,7 @@ use pocketmine\world\generator\populator\Ore;
 use pocketmine\world\generator\populator\Populator;
 use pocketmine\world\World;
 
-class Normal extends Generator{
+class OverworldGenerator extends Generator{
 
 	private int $waterHeight = 62;
 	/** @var Populator[] */
@@ -110,6 +110,7 @@ class Normal extends Generator{
 		$stone = VanillaBlocks::STONE();
 		$ores->setOreTypes([
 			new OreType(VanillaBlocks::COAL_ORE(), $stone, 20, 16, 0, 128),
+	                new OreType(VanillaBlocks::DIAMOND(), $stone, 20, 32, 0, 128),
 			new OreType(VanillaBlocks::IRON_ORE(), $stone, 20, 8, 0, 64),
 			new OreType(VanillaBlocks::REDSTONE_ORE(), $stone, 8, 7, 0, 16),
 			new OreType(VanillaBlocks::LAPIS_LAZULI_ORE(), $stone, 1, 6, 0, 32),
